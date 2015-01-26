0715f400c2e08357f21b722ff598ed8dd329df9e
Peng Shensong <pss9204@hotmail.com>
2015-01-26 18:31:27 +0800

    Change format of create organisation blade

--- app/views/users/create_organisation.blade.php
+++ app/views/users/create_organisation.blade.php
@@ -1,5 +1,10 @@
-@extends('layouts.default')
+@extends('layouts.portal')
 @section('body')
+
+{{ HTML::style('/css/font-awesome.css') }}
+{{ HTML::style('/css/style.css') }}
+{{ HTML::style('/css/responsive.css') }}
+
 <div class="row">
     <div class="col-md-4 col-md-offset-4">
         <h2>Sign Up An Organisation</h2>
