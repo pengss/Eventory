a9be31cc1481d8e94fb02c7a6c5c9f27db34fc08
jervenne <j0y5@hotmail.com>
2015-02-02 03:06:57 +0800

    UI: success sponsor

--- app/views/sponsor/success_payment.blade.php
+++ app/views/sponsor/success_payment.blade.php
@@ -1,1 +1,26 @@
-C:\xampp\htdocs\Eventory\app/views/sponsor/success_payment.blade.php
+@extends('layouts.main')
+@section('body')
+
+<!-- Full Background -->
+        <!-- For best results use an image with a resolution of 1280x1280 pixels (prefer a blurred image for smaller file size) -->
+        <img src="images/error_full_bg.jpg" alt="Full Background" class="full-bg full-bg-bottom animation-pulseSlow">
+        <!-- END Full Background -->
+
+        <!-- Error Container -->
+        <div id="error-container">
+            <div class="row text-center">
+                <div class="col-md-8 col-md-offset-2">
+                    <h1 class="text-light animation-fadeInQuick"><strong>Sponsorship request success!</strong></h1>
+                    <h2 class="text-muted animation-fadeInQuickInv"><em>Your sponsorship request has been sent to the event organizer and we'll be helping to liase with you shortly.</em></h2>
+                </div>
+                <div class="row gallery2">
+                            
+                        </div>
+                        <!-- END Gallery Items -->
+                        <a href="{{ URL::to('sponsorProfile') }}" class="btn btn-effect-ripple btn-primary"><i class="fa fa-arrow-left"></i>Dashboard</a>
+                        <a href="javascript:history.back()" class="btn btn-warning" style="margin-left: 48px;"><i class="gi gi-kiosk"></i> Back to Event page</a>
+                 
+            </div>
+        </div>
+        <!-- END Error Container -->
+@stop
