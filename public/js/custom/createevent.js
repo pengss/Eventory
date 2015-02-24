5fde6e41230821631de319e3f41fe819f4d8c2ae
Peng Shensong <pss9204@hotmail.com>
2015-02-25 02:06:37 +0800

    Revert "Revert "UI: create event validation""
    This reverts commit 122c24f552649655181b5eff8b547c64f11c2f6b.

--- public/js/custom/createevent.js
+++ public/js/custom/createevent.js
@@ -1,27 +1,43 @@
 $("input[id=target_audience]").change(function(){
-        var max= 3;
-        if( $("input[id=target_audience]:checked").length == max ){
-            $("input[id=target_audience]").attr('disabled', 'disabled');
-            $("input[id=target_audience]:checked").removeAttr('disabled');
-        }else{
-         $("input[id=target_audience]").removeAttr('disabled');
-     }
- })
+    var max= 3;
+    if( $("input[id=target_audience]:checked").length == max ){
+        $("input[id=target_audience]").attr('disabled', 'disabled');
+        $("input[id=target_audience]:checked").removeAttr('disabled');
+    }else{
+       $("input[id=target_audience]").removeAttr('disabled');
+   }
+})
 
-    $("input[id=event_types]").change(function(){
-        var max= 3;
-        if( $("input[id=event_types]:checked").length == max ){
-            $("input[id=event_types]").attr('disabled', 'disabled');
-            $("input[id=event_types]:checked").removeAttr('disabled');
-        }else{
-         $("input[id=event_types]").removeAttr('disabled');
-     }
- })
-    $("#no_presence").click(function () {
-    if ($(this).prop('checked') === true) {
-        $('.presenceTable').hide();
-    } 
-    if ($(this).prop('checked') === false) {
-        $('.presenceTable').show();
-    } 
+$("input[id=event_types]").change(function(){
+    var max= 3;
+    if( $("input[id=event_types]:checked").length == max ){
+        $("input[id=event_types]").attr('disabled', 'disabled');
+        $("input[id=event_types]:checked").removeAttr('disabled');
+    }else{
+       $("input[id=event_types]").removeAttr('disabled');
+   }
+})
+
+
+ $('#clickable-wizard').parsley();
+
+  $('.next').on('click', function () {
+    var current = $(this).data('currentBlock'),
+    next = $(this).data('nextBlock');
+
+    // only validate going forward. If current group is invalid, do not go further
+    // .parsley().validate() returns validation result AND show errors
+    if (next > current)
+      if (false === $('#clickable-wizard').parsley().validate('block' + current))
+        return;
+
+    // validation was ok. We can go on next step.
+    $('.block' + current)
+    .removeClass('show')
+    .addClass('hidden');
+
+    $('.block' + next)
+    .removeClass('hidden')
+    .addClass('show');
+
 })
