ad68f8c847b26cd4fef5ee615324c6ee7d3c7ee0
Peng Shensong <pss9204@hotmail.com>
2015-03-09 01:27:33 +0800

    Added the two interest blades for email

---
+++ app/views/emails/event_organiser_interest.blade.php
@@ -0,0 +1,10 @@
+<h2> Hi Eventory, </h2>
+
+<p> The following Event Organiser has expressed interest in signing up with us </p>
+
+<p>
+	Name: {{$name}} <br>
+	Event Name: {{$eventName}}<br>
+	Event Date: {{$date}}<br>
+	Email: {{$mail}}
+</p>
---
+++ app/views/emails/sponsor_interest.blade.php
@@ -0,0 +1,9 @@
+<h2> Hi Eventory, </h2>
+
+<p> The following Sponsor has expressed interest in signing up with us </p>
+
+<p>
+	Name: {{$name}} <br>
+	Organisation Name: {{$organisation}}<br>
+	Email: {{$mail}}
+</p>
